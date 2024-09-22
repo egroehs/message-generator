9673b8cbc6d9f534942b87d7f9046a5eedeb22b3
egroehs <eduardagroehs@gmail.com>
2024-09-22 14:59:43 -0700

    backend and frontend

---
+++ spotify-generator/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from "react";
+import RandomTrack from "./random";
+
+const App = () => {
+  return (
+    <div className="App">
+      <RandomTrack />
+    </div>
+  );
+};
+
+export default App;
