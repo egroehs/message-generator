d092d6de12876332e0b7b6888aa2f6bd3be231d5
egroehs <eduardagroehs@gmail.com>
2024-09-22 14:06:36 -0700

    first commit

---
+++ message-generator/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import RandomTrack from "./randomTrack";
+
+const App = () => {
+  return (
+    <div className="App">
+      <RandomTrack />
+    </div>
+  );
+};
+
+export default App;
